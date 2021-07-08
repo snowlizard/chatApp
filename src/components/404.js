f5c9c55d76458f688fbf2945299d8ef38453cb5e
snowlizard <illegal_burrito@live.com>
2021-07-08 14:04:02 -0700

    styling and adding 404 page

---
+++ src/components/404.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export const e404 = () => {
+    return(
+        <div>
+            <h1>Error 404!</h1>
+            <h2>Either your lost or we are lmao</h2>
+        </div>
+    );
+}
