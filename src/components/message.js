0e42365653a2faaf7db0ac8d5429f622cdf976c4
snowlizard <illegal_burrito@live.com>
2021-07-10 16:33:53 -0700

    working on message.js .scss

--- src/components/message.js
+++ src/components/message.js
@@ -1,13 +1,34 @@
 import React from 'react';
 
 export const Message = () => {
+
+    const msg = () => {
+        const container = document.getElementById('msgInput');
+        container.hidden = false;
+    }
+
+    const cancel = () => {
+        const container = document.getElementById('msgInput');
+        container.hidden = true;
+    }
+
     return (
         <div id="msgContainer">
             <div id="backgroundContainer">
                 <div id="messageArea"></div>
-                <div className="messageInput">
-                    <textarea id="inputArea" cols="20" rows="1"></textarea>
-                    <button id="sendMsg">Send</button>
+            </div>
+            <div className="messageInput" id="msgInput" hidden={true}>
+                    <div className="innerDiv">
+                        <textarea id="inputArea"></textarea>
+                    </div>
+                    <div className="innerDiv">
+                        <button id="sendMsg">Send</button>
+                        <button onClick={cancel} id="cancelSend">Cancel</button>
+                    </div>
+                </div>
+            <div id="sideBar">
+                <div className="sideBtn">
+                    <button onClick={msg}>Msg</button>
                 </div>
             </div>
         </div>
