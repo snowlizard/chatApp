aa203572e69f65faa63860a55a8dadc31322da6e
snowlizard <illegal_burrito@live.com>
2022-01-07 15:24:01 -0800

    some refactoring added link name, image functionality

--- src/pages/msg.js
+++ src/pages/msg.js
@@ -2,27 +2,6 @@ import React from 'react';
 
 export const Msg = (props) => {
     const msgClass = props.uid === props.currentUser ? 'sent' : 'recieved';
-    const link = new RegExp(/http[^\s]+/);
-    const image = new RegExp(/^(<img)/);
-
-    const ConvertLink = () => {
-        const textArray = props.text.split(' ');
-
-        return (
-            <p>
-                {
-                    textArray.map( word => {
-                        if(link.test(word)){
-                            return <a key={word} href={word} target='_blank'>{word}</a>
-                        }else{
-                            return ` ${word} `
-                        }
-                    })
-                }
-            </p>
-        );
-
-    }
 
     return(
         <div id="cont" className={msgClass}>
@@ -34,12 +13,44 @@ export const Msg = (props) => {
                 </p>
                 <div id="textBubble">
                     <div>
-                        {
-                            <ConvertLink/>
-                        }
+                        { <MsgText message={props.text}/> }
                     </div>
                 </div>
             </div>
         </div>
     );
+}
+
+const MsgText = (props) => {
+    const link = new RegExp(/http[^\s]+/);
+    const linkName = new RegExp(/>?!\w+/);
+    const image = new RegExp(/>?!img/);
+    const textArray = props.message.split(' ');
+    
+    return (
+        <p>
+            {
+                textArray.map( word => {
+                    if(image.test(word)){
+                        const imageStr = word.split('!');
+                        return (<a key={word} href={imageStr[0]} target='_blank'
+                                   className='imageLink'>
+                                    <img key={word} src={imageStr[0]} ></img>
+                                </a>);
+                    }else if (linkName.test(word)){
+                        const linkArray = word.split('!');
+                        return(<a key={word} href={linkArray[0]} target="_blank">
+                                {linkArray[1]}
+                               </a>)
+                    }
+                    else if (link.test(word)){
+                        return <a key={word} href={word} target='_blank'>{word}</a>
+                    }else{
+                        return ` ${word} `
+                    }
+                })
+            }
+        </p>
+    );
+
 }
